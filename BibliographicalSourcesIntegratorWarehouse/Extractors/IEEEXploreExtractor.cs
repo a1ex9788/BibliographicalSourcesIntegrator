7d08b187e5d7eb68a20d1cfacdad6a6669c16e14
Fracoped <franciscoconejospedro@gmail.com>
2020-11-03 17:13:48 +0100

    IEEEXplorer extractor 1.0

--- BibliographicalSourcesIntegratorWarehouse/Extractors/IEEEXploreExtractor.cs
+++ BibliographicalSourcesIntegratorWarehouse/Extractors/IEEEXploreExtractor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BibliographicalSourcesIntegratorWarehouse.Entities;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,72 +11,65 @@ namespace BibliographicalSourcesIntegratorWarehouse.Extractors
     {
         public void ExtractData(string json)
         {
-                try
+            try
+            {
+                string preparedJson = PrepareJson(json);
+
+                List<IEEEXplorerPublicationSchema> publications = JsonConvert.DeserializeObject<List<IEEEXplorerPublicationSchema>>(preparedJson);
+
+                foreach (IEEEXplorerPublicationSchema IEEPublication in publications)
                 {
-            //        string preparedJson = PrepareJson(json);
-            //
-            //        List<DBLPPublicationSchema> publications = JsonConvert.DeserializeObject<List<DBLPPublicationSchema>>(preparedJson);
-            //
-            //        foreach (DBLPPublicationSchema dBLPPublication in publications)
-            //        {
-            //            Article publication = new Article(
-            //                title: dBLPPublication.title,
-            //                year: dBLPPublication.year,
-            //                url: dBLPPublication.url,
-            //                people: null,
-            //                initialPage: GetInitialPage(dBLPPublication.pages),
-            //                finalPage: GetFinalPage(dBLPPublication.pages),
-            //                exemplar: null
-            //            );
-            //        }
+                    Article publication = new Article(
+                    title: IEEPublication.title,
+                    year: IEEPublication.publication_year,
+                    url: IEEPublication.pdf_url,
+                    people: null,
+                    initialPage: Convert.ToInt32(IEEPublication.start_page),
+                    finalPage: Convert.ToInt32(IEEPublication.end_page),
+                    exemplar: new Exemplar(Convert.ToInt32(IEEPublication.volume), Convert.ToInt32(IEEPublication.article_number), 1, null, null));
                 }
-                catch (Exception e) { }
+
+            }
+
+            catch (Exception e) { }
         }
-        //
-        //static string PrepareJson(string source)
-        //{
-        //    string aux = source;
-        //
-        //    string jsonWithoutSpecialChars = aux.Replace("\t", "").Replace("\n", "").Replace("\r", "").Replace("$", "dollar");
-        //
-        //    string jsonWithoutDBLPNode = jsonWithoutSpecialChars.Substring(21);
-        //
-        //    string jsonWithoutEndDBLPNodeBracket = jsonWithoutDBLPNode.Substring(0, jsonWithoutDBLPNode.Length - 2);
-        //
-        //    //string jsonWithSquareBrackets = AddSquareBrackets(jsonWithoutEndDBLPNodeBracket);
-        //
-        //    return jsonWithSquareBrackets;
-        //}
-    }
+
+        static string PrepareJson(string json)
+        {
+            string aux = json;
+            string jsonWithoutSpecialChars = aux.Replace("\t", "").Replace("\n", "").Replace("\r", "");
+            string jsonWithoutRootNode = jsonWithoutSpecialChars.Substring(68);
+            // jsonWithoutRootNode = '[' + jsonWithoutRootNode;
+            return jsonWithoutRootNode;
+        }
+
+        class IEEEXplorerPublicationSchema
+        {
+            public string title { get; set; }
+
+            public string publisher { get; set; }
+
+            public string volume { get; set; }
+
+            public List<Object> authors { get; set; }
+
+            public string publication_title { get; set; }
+
+            public int publication_year { get; set; }
+
+            public string start_page { get; set; }
+
+            public string end_page { get; set; }
+
+            public string pdf_url { get; set; }
+
+            public string article_number { get; set; }
+
+            public string publication_date { get; set; }
 
 
-    class IEEEXplorerPublicationSchema
-    {
-        public string title { get; set; }
-    
-        public string publisher { get; set; }
-    
-        public string volume { get; set; }
-    
-        public List<Object> authors { get; set; }
-    
-        public string publication_title { get; set; }
-    
-        public int publication_year { get; set; }
-    
-        public string start_page { get; set; }
-    
-        public string end_page { get; set; }
-        
-        public string pdf_url { get; set; }
-    
-        public string article_number { get; set; }
-    
-        public string publication_date { get; set; }
-    
-    
-    
-    
+
+
+        }
     }
 }
-
