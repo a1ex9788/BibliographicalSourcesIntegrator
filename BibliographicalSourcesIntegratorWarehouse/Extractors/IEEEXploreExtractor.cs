c2285e9fd3b4d7693b82ed726baf02c2d32c3644
Fracoped <franciscoconejospedro@gmail.com>
2020-11-04 20:09:03 +0100

    IEEEXtractor "acabado"

--- BibliographicalSourcesIntegratorWarehouse/Extractors/IEEEXploreExtractor.cs
+++ BibliographicalSourcesIntegratorWarehouse/Extractors/IEEEXploreExtractor.cs
@@ -4,44 +4,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BibliographicalSourcesIntegratorWarehouse.Persistence;
 
 namespace BibliographicalSourcesIntegratorWarehouse.Extractors
 {
     public class IEEEXploreExtractor
     {
+        private readonly PublicationCreator publicationConstructor;
+        private readonly DatabaseAccess databaseAccess;
+
+
+        public IEEEXploreExtractor(PublicationCreator publicationConstructor, DatabaseAccess databaseAccess)
+        {
+            this.publicationConstructor = publicationConstructor;
+            this.databaseAccess = databaseAccess;
+        }
+
         public void ExtractData(string json)
         {
-            try
-            {
                 string preparedJson = PrepareJson(json);
 
                 List<IEEEXplorerPublicationSchema> publications = JsonConvert.DeserializeObject<List<IEEEXplorerPublicationSchema>>(preparedJson);
 
+                List<Article> articles = new List<Article>();
+
                 foreach (IEEEXplorerPublicationSchema IEEPublication in publications)
                 {
-                    Journal journal = new Journal(
-                        name: IEEPublication.publisher
-                        );
-                    Exemplar exemplar = new Exemplar(
-                        //volume: Convert.ToInt32(IEEPublication.volume),
-                        volume: "",
-                        number: Convert.ToInt32(IEEPublication.article_number),
-                        month: getMonth(IEEPublication.publication_date),
-                        journal: journal
-                        );
-                    // Article publication = new Article(
-                    // title: IEEPublication.title,
-                    // year: IEEPublication.publication_year,
-                    // url: IEEPublication.pdf_url,
-                    // people: null,
-                    // initialPage: Convert.ToInt32(IEEPublication.start_page),
-                    // finalPage: Convert.ToInt32(IEEPublication.end_page),
-                    // exemplar: new Exemplar(Convert.ToInt32(IEEPublication.volume), Convert.ToInt32(IEEPublication.article_number), 1, null, null));
+                    articles.Add(publicationConstructor.CreateArticle(
+                    title: IEEPublication.title,
+                    year: IEEPublication.publication_year,
+                    url: IEEPublication.pdf_url,
+                    authors: IEEPublication.authors.GetAuthors(),
+                    initialPage: Convert.ToInt32(IEEPublication.start_page),
+                    finalPage: Convert.ToInt32(IEEPublication.end_page),
+                    volume: IEEPublication.volume,
+                    number: Convert.ToInt32(IEEPublication.article_number),
+                    month: getMonth(IEEPublication.publication_date),
+                    journalName: IEEPublication.publisher));
+
                 }
 
-            }
-
-            catch (Exception e) { }
+                databaseAccess.SaveArticles(articles);
         }
 
         static string PrepareJson(string json)
@@ -148,6 +151,91 @@ namespace BibliographicalSourcesIntegratorWarehouse.Extractors
         {
             public List<Object> authors { get; set; }
 
+
+            public List<(string name, string surnames)> GetAuthors()
+            {
+                List<(string name, string surnames)> authorsFinal = new List<(string name, string surnames)>();
+
+                // o --> {{ "authorUrl": "https://ieeexplore.ieee.org/author/37600583700", "id": 37600583700,"full_name": "Ying-Nong Chen","author_order": 1}}
+                foreach (Object o in authors)
+                {
+                    string name = "";
+                    string surnames = "";
+                    string completeName = "";
+           
+                    
+                    Author2 author = JsonConvert.DeserializeObject<Author2>(o.ToString());
+                    completeName = author.full_name;
+
+                    name = GetName(completeName);
+                    surnames = GetSurnames(completeName);
+           
+                    authors.Add((name, surnames));
+                }
+           
+                return authorsFinal;
+           
+           
+                string GetName(string completeName)
+                {
+
+                    string[] splittedName = completeName.Split(' ');
+
+                    if (splittedName[1].Contains('.'))
+                    {
+                        return splittedName.Length > 1 ? splittedName[0] + splittedName[1] : splittedName[0];
+                    }
+                    else
+                    {
+                        return splittedName[0];
+                    }
+                }
+               
+                string GetSurnames(string completeName)
+                {
+                    string[] splittedName = completeName.Split(' ');
+                    string surnames = "";
+
+                    if (splittedName[1].Contains('.'))
+                    {
+                        for (int i = 2; i < splittedName.Length; i++)
+                        {
+                            surnames += splittedName[i] + " ";
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 1; i < splittedName.Length; i++)
+                        {
+                            surnames += splittedName[i] + " ";
+                        }
+                    }
+
+                    return surnames.TrimEnd();
+                }
+            
+            }
         }
+
+        class Author2
+        {
+            string authorUrl;
+
+            public string full_name;
+
+            string id;
+
+            string author_order;
+
+            public Author2(string authorUrl, string full_name, string id, string author_order)
+            {
+                this.authorUrl = authorUrl;
+                this.full_name = full_name;
+                this.id = id;
+                this.author_order = author_order;
+            }
+        }
+
+
     }
 }
